f5cd2187d51c6f4688ceab2234cb0cd21c766531
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-03 18:23:45 +0100

    affichage web

--- src/graph.py
+++ src/graph.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
 
 from testData import trajet_en_df
 
 
-def representation_itineraire(data, reseau_neurones=[]):
+def representation_itineraire_back(data, reseau_neurones=[]):
     """Affichage des N villes par des points ainsi que le parcours réalisé
        Le parcours est donné par l'ordre des villes dans le dataframe
 
@@ -40,24 +41,50 @@ def representation_itineraire(data, reseau_neurones=[]):
     plt.show()
 
 
-def affichage(resolution, data):
+def representation_itineraire_web(data):
+    """Affichage des N villes par des points ainsi que le parcours réalisé
+       Le parcours est donné par l'ordre des villes dans le dataframe
+
+    Parameters
+    ----------
+    data : DataFrame
+        Dataframe stockant l'intégralité des informations sur un algorithme
+    """
+    fig = px.line(data, x='x', y='y')
+    fig.show()
+
+
+def representation_temps_calcul(data):
+    """Affichage des du temps de calcul d'un algorithme en fonction
+    du nombre de ville qu'il a traité
+
+    Parameters
+    ----------
+    data : DataFrame
+        Dataframe stockant l'intégralité des informations sur un algorithme
+    """
+    fig = px.line(data, x='Nombre de villes', y='Temps de calcul (en s)')
+    fig.show()
+
+
+def affichage(df_resolution, data):
     """Affichage d'un trajet et des performances d'un algorithme
 
     Parameters
     ----------
-    resolution : dict
+    df_resolution : Dataframe
         variable stockant un ensemble de variables importantes pour analyser
         l'algorithme
     data : DataFrame
         Dataframe stockant l'intégralité des coordonnées des villes à parcourir
     """
-    df_meilleur_trajet = trajet_en_df(resolution['Chemins'][-1], data)
-    representation_itineraire(df_meilleur_trajet)
+    df_meilleur_trajet = trajet_en_df(df_resolution.loc[0, 'Solution'], data)
+    # representation_itineraire(df_meilleur_trajet)
+    representation_itineraire_web(df_meilleur_trajet)
 
-    print("Analyse de la performance de l'algorithme :",
-          resolution["Algorithme"])
     print("=============================================")
-    print("Nombre de ville : ", resolution["Nombre de villes"])
-    print("Pourcentage d'erreur : ", resolution["Erreur (en %)"])
-    print("Temps de calcul (en s): ", resolution["Temps de calcul (en s)"])
+    print("Nombre de ville : ", df_resolution.loc[0, "Nombre de villes"])
+    print("Pourcentage d'erreur : ", df_resolution.loc[0, "Erreur (en %)"])
+    print("Temps de calcul (en s): ",
+          df_resolution.loc[0, "Temps de calcul (en s)"])
     print("=============================================")
