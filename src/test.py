56239c825643d442744e6101f45136ffba53e7be
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-17 20:55:21 +0100

    test

--- src/test.py
+++ src/test.py
@@ -1,12 +1,19 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial import distance
-from test_algo import test_unitaire_plus_proche_voisin
+from test_algo import test_unitaire_plus_proche_voisin, test_unitaire_2_opt
 from graph import representation_itineraire_web, representation_itineraire_back
 from init_test_data import data_TSPLIB, trajet_en_df
 
+"""
 data = data_TSPLIB('../data/qa194.tsp')
-df = test_unitaire_plus_proche_voisin(1)
+df = test_unitaire_plus_proche_voisin(0)
+df_solution = trajet_en_df(df['Solution'], data)
+representation_itineraire_web(df_solution).show()
+"""
+
+data = data_TSPLIB('../data/qa194.tsp')
+df = test_unitaire_2_opt(1)
 print(df)
 df_solution = trajet_en_df(df['Solution'], data)
 representation_itineraire_web(df_solution).show()
