450a74f570955f09e883f0eaad9ff5c01fdad318
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-18 16:16:23 +0100

    algo proche voisin fonctionnelle avec nouvelles data

--- src/algo_proche_voisin.py
+++ src/algo_proche_voisin.py
@@ -5,17 +5,15 @@ import pandas as pd
 
 from distance import distance_trajet
 from graph import affichage
-from init_test_data import data_TSPLIB, tour_optimal, trajet_en_df
+from init_test_data import data_TSPLIB, trajet_en_df
 
 
-def plus_proche_voisin(data, matrice_distance):
+def plus_proche_voisin(matrice_distance):
     """Retourne le trajet trouvé en se déplacement de proche en proche.
     La ville de départ étant arbitraire on choisit la ville d'index 0
 
     Parameters
     ----------
-    data : DataFrame
-        Dataframe stockant l'intégralité des coordonnées des villes à parcourir
     matrice_distance : list
         matrice stockant l'integralité des distances inter villes
 
@@ -28,34 +26,36 @@ def plus_proche_voisin(data, matrice_distance):
     """
     start_time = time.time()
 
-    # Stockage de la progression de l'exploration
-    chemin_explore = []
+    # Initialiation du matrice booléenne d'état de visite des villes
+    visite = np.zeros(len(matrice_distance)) != 0
+
+    # Initialisation de l'itinéraire
     itineraire = [0]
-    while len(data.loc['x']) != len(itineraire):
+    visite[0] = True
+
+    while False in visite:
         # A chaque itération on cherche la ville la plus proche de la ville actuelle
         # la ville actuelle étant la dernière de l'itinéraire
 
-        # Liste trié dans l'ordre croissant des distances entre la ville actuelle et le reste
-        distances = sorted(matrice_distance[itineraire[-1]])
+        # Pour ne pas modifier le paramètre matrice_distance
+        distance_a_ville = matrice_distance[itineraire[-1], :]
 
-        # On enlève la distance qui correspond à rester sur la même ville
-        distances.remove(0)
+        for index in range(len(distance_a_ville)):
+            if visite[index]:
+                distance_a_ville[index] = np.Inf
 
-        i = 0
-        # On recherche la ville la plus proche encore inexplorée
-        while matrice_distance[itineraire[-1]].index(distances[i]) in itineraire:
-            i += 1
-        itineraire.append(
-            matrice_distance[itineraire[-1]].index(distances[i]))
-        chemin_explore.append(itineraire)
+        plus_proche = np.argmin(distance_a_ville)
+
+        visite[plus_proche] = True
+        itineraire.append(plus_proche)
     # On fait attention à fermer le cycle
     itineraire.append(itineraire[0])
 
     temps_calcul = time.time() - start_time
-    return chemin_explore, temps_calcul
+    return itineraire, temps_calcul
 
 
-def main(data, matrice_distance, chemin_optimal=[]):
+def main(matrice_distance):
     """Lancement de l'algorithme de recherche 
 
     Parameters
@@ -65,8 +65,6 @@ def main(data, matrice_distance, chemin_optimal=[]):
     chemin_initial : list
         suite de villes donnant le chemin parcouru. Ce chemin initial influ énormément 
         sur la solution finale trouvée.
-    chemin_optimal : list
-        résulat optimal donné par la TSPLIB
 
     Returns
     -------
@@ -74,25 +72,16 @@ def main(data, matrice_distance, chemin_optimal=[]):
         variable stockant un ensemble de variables importantes pour analyser
         l'algorithme
     """
-
-    if chemin_optimal != []:
-        distance_chemin_optimal = distance_trajet(
-            chemin_optimal, matrice_distance)
-
     # On récupère les chemins testés et le temps de résolution de l'algorithme
-    chemin_explores, temps_calcul = plus_proche_voisin(data, matrice_distance)
+    itineraire, temps_calcul = plus_proche_voisin(matrice_distance)
 
     # Calcul de la distance du trajet final trouvé par l'algorithme. En dernière position
     # de la variable précédente
     distance_chemin_sub_optimal = distance_trajet(
-        chemin_explores[-1], matrice_distance)
-    # Calcul de l'erreur si un chemin optimal est renseigné
-    if chemin_optimal != []:
-        erreur = 100*(distance_chemin_sub_optimal -
-                      distance_chemin_optimal)/distance_chemin_optimal
+        itineraire, matrice_distance)
 
     # Chemin final trouvé
-    solution = chemin_explores[-1]
+    solution = itineraire
 
     # Création du dataframe à retourner
     df_resultat_test = pd.DataFrame({
@@ -100,7 +89,7 @@ def main(data, matrice_distance, chemin_optimal=[]):
         # Dans un tableau pour être sur une seule ligne du dataframe
         'Solution': [solution],
         # Erreur par rapport à la solution optimal de la TSPLIB
-        'Erreur (en %)': erreur,
+        'Distance': distance_chemin_sub_optimal,
         'Temps de calcul (en s)': temps_calcul
     })
 
