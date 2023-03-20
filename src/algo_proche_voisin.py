a7129ac7ba01e15c1bec72bb47c68f317b0650ea
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-20 11:10:05 +0100

    Annotations et commentaires

--- src/algo_proche_voisin.py
+++ src/algo_proche_voisin.py
@@ -8,20 +8,21 @@ from affichage_resultats import affichage
 from init_test_data import data_TSPLIB, trajet_en_df
 
 
-def plus_proche_voisin(matrice_distance):
+def plus_proche_voisin(matrice_distance: np.array) -> tuple(list, float):
     """Retourne le trajet trouvé en se déplacement de proche en proche.
+
     La ville de départ étant arbitraire on choisit la ville d'index 0
 
     Parameters
     ----------
-    matrice_distance : list
+    matrice_distance : np.array
         matrice stockant l'integralité des distances inter villes
 
     Returns
     -------
     chemin_explores : list
-        L'ensemble des sous chemins empruntés pour arriver au résulat
-    temps_calcul : int
+        le chemin finalement trouvé
+    temps_calcul : float
         temps necessaire à la résolution du problème
     """
     start_time = time.time()
@@ -37,16 +38,19 @@ def plus_proche_voisin(matrice_distance):
         # A chaque itération on cherche la ville la plus proche de la ville actuelle
         # la ville actuelle étant la dernière de l'itinéraire
 
-        # Pour ne pas modifier le paramètre matrice_distance
+        # Pour ne pas modifier les valeurs de matrice_distance
         distance_a_ville = np.copy(matrice_distance[itineraire[-1], :])
 
         for index in range(len(distance_a_ville)):
             if visite[index]:
                 distance_a_ville[index] = np.Inf
 
+        # Récupération de l'index de la ville la plus proche
         plus_proche = np.argmin(distance_a_ville)
 
+        # On donne l'état visité à la ville
         visite[plus_proche] = True
+
         itineraire.append(plus_proche)
     # On fait attention à fermer le cycle
     itineraire.append(itineraire[0])
@@ -55,16 +59,13 @@ def plus_proche_voisin(matrice_distance):
     return itineraire, temps_calcul
 
 
-def main(matrice_distance):
+def main(matrice_distance: np.array) -> pd.DataFrame:
     """Lancement de l'algorithme de recherche 
 
     Parameters
     ----------
-    matrice_distance : list
+    matrice_distance : np.array
         matrice stockant l'integralité des distances inter villes
-    chemin_initial : list
-        suite de villes donnant le chemin parcouru. Ce chemin initial influ énormément 
-        sur la solution finale trouvée.
 
     Returns
     -------
@@ -72,13 +73,11 @@ def main(matrice_distance):
         variable stockant un ensemble de variables importantes pour analyser
         l'algorithme
     """
-    # On récupère les chemins testés et le temps de résolution de l'algorithme
+    # On récupère le chemin trouvé et le temps de résolution de l'algorithme
     itineraire, temps_calcul = plus_proche_voisin(matrice_distance)
 
-    # Calcul de la distance du trajet final trouvé par l'algorithme. En dernière position
-    # de la variable précédente
-    distance_chemin_sub_optimal = distance_trajet(
-        itineraire, matrice_distance)
+    # Calcul de la distance du trajet final trouvé par l'algorithme
+    distance_chemin_sub_optimal = distance_trajet(itineraire, matrice_distance)
 
     # Chemin final trouvé
     solution = itineraire
@@ -89,7 +88,7 @@ def main(matrice_distance):
         'Nombre de villes': len(solution),
         # Dans un tableau pour être sur une seule ligne du dataframe
         'Solution': [solution],
-        # Erreur par rapport à la solution optimal de la TSPLIB
+        # Distance du trajet final
         'Distance': distance_chemin_sub_optimal,
         'Temps de calcul (en s)': temps_calcul
     })
