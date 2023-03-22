fc3efb156ee9327982cd79c102ce548c882dfaac
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-22 20:18:03 +0100

    affichage réseau de kohonen

--- src/affichage_resultats.py
+++ src/affichage_resultats.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -40,6 +41,58 @@ def representation_itineraire_back(data: pd.DataFrame, reseau_neurones=[]):
         plt.plot(x, y, zorder=1)
         plt.title('Chemin parcouru par le réseau', loc='center')
     plt.show()
+
+
+def plot_network(cities, neurons, name='diagram.png', ax=None):
+    """Plot a graphical representation of the problem"""
+    mpl.rcParams['agg.path.chunksize'] = 10000
+
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon=False)
+        axis = fig.add_axes([0, 0, 1, 1])
+
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
+
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        axis.plot(neurons[:, 0], neurons[:, 1], 'r.',
+                  ls='-', color='#0063ba', markersize=2)
+
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        ax.plot(neurons[:, 0], neurons[:, 1], 'r.',
+                ls='-', color='#0063ba', markersize=2)
+        return ax
+
+
+def plot_route(cities, route, name='diagram.png', ax=None):
+    """Plot a graphical representation of the route obtained"""
+    mpl.rcParams['agg.path.chunksize'] = 10000
+
+    if not ax:
+        fig = plt.figure(figsize=(5, 5), frameon=False)
+        axis = fig.add_axes([0, 0, 1, 1])
+
+        axis.set_aspect('equal', adjustable='datalim')
+        plt.axis('off')
+
+        axis.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        axis.plot(route['x'], route['y'], color='purple', linewidth=1)
+
+        plt.savefig(name, bbox_inches='tight', pad_inches=0, dpi=200)
+        plt.close()
+
+    else:
+        ax.scatter(cities['x'], cities['y'], color='red', s=4)
+        route = cities.reindex(route)
+        route.loc[route.shape[0]] = route.iloc[0]
+        ax.plot(route['x'], route['y'], color='purple', linewidth=1)
+        return ax
 
 
 def representation_itineraire_web(data: pd.DataFrame) -> px.line:
