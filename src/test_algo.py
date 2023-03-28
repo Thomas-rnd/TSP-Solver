48a17edcea2e37700157ab4d22d70b3f50c21667
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-28 20:47:53 +0200

    Ajout chemins explorés + nom dataset

--- src/test_algo.py
+++ src/test_algo.py
@@ -6,14 +6,14 @@ import src.algo_proche_voisin
 import src.algo_kohonen
 from src.distance import matrice_distance
 from src.init_test_data import data_TSPLIB
-from src.affichage_resultats import affichage
+from src.affichage_resultats import affichage, affichage_chemins_explores
 
 # Nom des data de test
 ENSEMBLE_TEST = ['dj38', 'xqf131', 'qa194', 'xqg237',
                  'pma343', 'pka379', 'pbl395', 'pbk411', 'pbn423']
 
 # Nom des algo implémentés
-ENSEMBLE_ALGOS = ['2_opt', 'plus_proche_voisin', 'genetique', 'kohonen']
+ENSEMBLE_ALGOS = ['2-opt', 'plus_proche_voisin', 'genetique', 'kohonen']
 
 
 def test_global(algo: str) -> pd.DataFrame:
@@ -36,18 +36,19 @@ def test_global(algo: str) -> pd.DataFrame:
     # Dataframe à retourner, une ligne représente un test de l'algorithme
     df_resultat_test = pd.DataFrame({
         'Algorithme': [],
+        'Nom dataset': [],
         'Nombre de villes': [],
         'Solution': [],
+        'Chemins explorés': [],
         # Distance du trajet final
         'Distance': [],
         'Temps de calcul (en s)': []
     })
-
     # Test sur l'ensemble des data
     for num_dataset in range(len(ENSEMBLE_TEST)):
         # Feeback d'avancement
         print(f"Etape du test : {num_dataset+1}/{len(ENSEMBLE_TEST)}")
-        if algo == '2_opt':
+        if algo == '2-opt':
             df_res = test_unitaire(num_dataset, algo)
         elif algo == 'plus_proche_voisin':
             df_res = test_unitaire(num_dataset, algo)
@@ -87,33 +88,27 @@ def test_unitaire(num_dataset: int, algo: str) -> pd.DataFrame:
     # Initialisation de la matrice des distances relatives
     mat_distance = matrice_distance(data)
 
-    if algo == '2_opt':
+    if algo == '2-opt':
         # On prend un chemin initial meilleur qu'un chemin aléatoire
         # Attention chemin_initial est la liste des chemins explorés par l'algorithme
         # plus_proche_voisin
-        chemin_initial, temps_calcul = src.algo_proche_voisin.plus_proche_voisin(
+        chemin_initial, _, _ = src.algo_proche_voisin.plus_proche_voisin(
             mat_distance)
 
         # Lancement de l'algorithme 2-opt
-        df_res = src.algo_2_opt.main(mat_distance, chemin_initial)
-        # Affichage du chemin trouvé et sauvegarde de la figure
-        affichage(df_res, data, f'{algo}/chemin_{ENSEMBLE_TEST[num_dataset]}')
+        df_res = src.algo_2_opt.main(
+            mat_distance, chemin_initial, ENSEMBLE_TEST[num_dataset])
     elif algo == 'plus_proche_voisin':
         # Lancement de l'algorithme plus proche voisin
-        df_res = src.algo_proche_voisin.main(mat_distance)
-        # Affichage du chemin trouvé et sauvegarde de la figure
-        affichage(df_res, data,
-                  f'{algo}/chemin_{ENSEMBLE_TEST[num_dataset]}')
+        df_res = src.algo_proche_voisin.main(
+            mat_distance, ENSEMBLE_TEST[num_dataset])
     elif algo == 'genetique':
         # Lancement de l'algorithme génétique
-        df_res = src.algo_genetique.main(data, mat_distance)
-        # Affichage du chemin trouvé et sauvegarde de la figure
-        affichage(df_res, data,
-                  f'{algo}/chemin_{ENSEMBLE_TEST[num_dataset]}')
+        df_res = src.algo_genetique.main(
+            data, mat_distance, ENSEMBLE_TEST[num_dataset])
     else:
         # Lancement de l'algorithme génétique
-        df_res = src.algo_kohonen.main(data, mat_distance)
-        # Affichage du chemin trouvé et sauvegarde de la figure
-        affichage(df_res, data, f'{algo}/chemin_{ENSEMBLE_TEST[num_dataset]}')
+        df_res = src.algo_kohonen.main(
+            data, mat_distance, ENSEMBLE_TEST[num_dataset])
 
     return df_res
