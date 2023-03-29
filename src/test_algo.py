e34f44350745f17480aed555e829475feef32627
Thomas RENAUD <renaudthomas@orange.fr>
2023-03-29 21:04:42 +0200

    Ajout exploration

--- src/test_algo.py
+++ src/test_algo.py
@@ -1,12 +1,13 @@
+import numpy as np
 import pandas as pd
 
 import src.algo_2_opt
 import src.algo_genetique
-import src.algo_proche_voisin
 import src.algo_kohonen
+import src.algo_proche_voisin
+from src.affichage_resultats import affichage, affichage_chemins_explores
 from src.distance import matrice_distance
 from src.init_test_data import data_TSPLIB
-from src.affichage_resultats import affichage, affichage_chemins_explores
 
 # Nom des data de test
 ENSEMBLE_TEST = ['dj38', 'xqf131', 'qa194', 'xqg237',
@@ -16,68 +17,74 @@ ENSEMBLE_TEST = ['dj38', 'xqf131', 'qa194', 'xqg237',
 ENSEMBLE_ALGOS = ['2-opt', 'plus_proche_voisin', 'genetique', 'kohonen']
 
 
-def test_global(algo: str) -> pd.DataFrame:
+def test_global(algorithme: str) -> pd.DataFrame:
     """Lancement de tous les tests unitaires pour un algorithme
 
     Parameters
     ----------
     algo : str
-        le nom de l'algorithme à utiliser
+        le nom de l'algorithme à utiliser parmi `['2-opt', 'plus_proche_voisin', 'genetique', 'kohonen']`
 
     Returns
     -------
     Dataframe 
-        variable stockant un ensemble de données importantes pour analyser
-        l'algorithme. Un ligne représente un test sur un jeu de données
+        Données retourné sur l'algorithme : 
+        `'Algorithme', 'Nom dataset', 'Nombre de villes', 'Solution', 'Distance', 'Temps de calcul (en s)'`
     """
-    assert algo in ENSEMBLE_ALGOS, print(
+    assert algorithme in ENSEMBLE_ALGOS, print(
         "Veuillez choisir un algorithme parmi : {}".format(ENSEMBLE_ALGOS))
 
     # Dataframe à retourner, une ligne représente un test de l'algorithme
     df_resultat_test = pd.DataFrame({
+        # Nom de l'algorithme
         'Algorithme': [],
+        # Nom du jeu de données
         'Nom dataset': [],
+        # Nombre de villes du jeu de données
         'Nombre de villes': [],
+        # Chemin le plus court trouvé
         'Solution': [],
-        'Chemins explorés': [],
         # Distance du trajet final
         'Distance': [],
+        # Temps mis par l'algorithme pour trouver le résultat
         'Temps de calcul (en s)': []
     })
-    # Test sur l'ensemble des data
+    # Test sur l'ensemble des jeux de données
     for num_dataset in range(len(ENSEMBLE_TEST)):
         # Feeback d'avancement
         print(f"Etape du test : {num_dataset+1}/{len(ENSEMBLE_TEST)}")
-        if algo == '2-opt':
-            df_res = test_unitaire(num_dataset, algo)
-        elif algo == 'plus_proche_voisin':
-            df_res = test_unitaire(num_dataset, algo)
-        elif algo == 'genetique':
-            df_res = test_unitaire(num_dataset, algo)
+        if algorithme == '2-opt':
+            df_res, _ = test_unitaire(num_dataset, algorithme)
+        elif algorithme == 'plus_proche_voisin':
+            df_res, _ = test_unitaire(num_dataset, algorithme)
+        elif algorithme == 'genetique':
+            df_res, _ = test_unitaire(num_dataset, algorithme)
         else:
-            df_res = test_unitaire(num_dataset, algo)
+            df_res, _ = test_unitaire(num_dataset, algorithme)
         df_resultat_test = pd.concat(
-            [df_resultat_test, df_res], ignore_index=True)  # type: ignore
+            [df_resultat_test, df_res], ignore_index=True)
 
     return df_resultat_test
 
 
-def test_unitaire(num_dataset: int, algo: str) -> pd.DataFrame:
+def test_unitaire(num_dataset: int, algo: str) -> tuple[pd.DataFrame, list[list[int]] | list[np.ndarray]]:
     """Lancement d'un test unitaire pour un algorithme
 
     Parameters
     ----------
     num_dataset : int
         numéro du dataset sur lequel est réalisé le test. Ce numéro est égale à son 
-        index dans ENSEMBLE_TEST
+        index dans `ENSEMBLE_TEST`
     algo : str
         le nom de l'algorithme à utiliser
 
     Returns
     -------
-    Dataframe
-        variable stockant un ensemble de données importantes pour analyser
-        l'algorithme
+    df_res : Dataframe
+        Données retourné sur l'algorithme sur un jeu de données : 
+        `'Algorithme', 'Nom dataset', 'Nombre de villes', 'Solution', 'Distance', 'Temps de calcul (en s)'`
+    exploration : list[list[int]] | list[np.ndarray]
+        variable stockant l'évolution de la recherche de l'algorithme
     """
     assert algo in ENSEMBLE_ALGOS, print(
         "Veuillez choisir un algorithme parmi : {}".format(ENSEMBLE_ALGOS))
@@ -90,25 +97,28 @@ def test_unitaire(num_dataset: int, algo: str) -> pd.DataFrame:
 
     if algo == '2-opt':
         # On prend un chemin initial meilleur qu'un chemin aléatoire
-        # Attention chemin_initial est la liste des chemins explorés par l'algorithme
-        # plus_proche_voisin
         chemin_initial, _, _ = src.algo_proche_voisin.plus_proche_voisin(
             mat_distance)
-
         # Lancement de l'algorithme 2-opt
-        df_res = src.algo_2_opt.main(
+        df_res, exploration = src.algo_2_opt.main(
             mat_distance, chemin_initial, ENSEMBLE_TEST[num_dataset])
+
     elif algo == 'plus_proche_voisin':
         # Lancement de l'algorithme plus proche voisin
-        df_res = src.algo_proche_voisin.main(
+        df_res, exploration = src.algo_proche_voisin.main(
             mat_distance, ENSEMBLE_TEST[num_dataset])
+
     elif algo == 'genetique':
         # Lancement de l'algorithme génétique
         df_res = src.algo_genetique.main(
             data, mat_distance, ENSEMBLE_TEST[num_dataset])
+        # Il n'y a pas de réelle méthode d'exploration : le fruit du hasard
+        # Exploration est donc initialisée à une liste vide pour conserver le bon type
+        exploration = []
+
     else:
-        # Lancement de l'algorithme génétique
-        df_res = src.algo_kohonen.main(
+        # Lancement de l'algorithme de kohonen
+        df_res, exploration = src.algo_kohonen.main(
             data, mat_distance, ENSEMBLE_TEST[num_dataset])
 
-    return df_res
+    return df_res, exploration
